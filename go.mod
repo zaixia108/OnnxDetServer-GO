58eabe212f8fe8f41c3a65626456b40ab5989d16
zaixia108 <xvbowen2012@gmail.com>
2025-12-10 22:25:48 +0800

    修正gRPC结构，添加log模块，使用python测试通过

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25
 require (
 	github.com/gin-gonic/gin v1.11.0
 	github.com/google/uuid v1.6.0
+	go.uber.org/zap v1.27.1
 	gocv.io/x/gocv v0.42.0
 	google.golang.org/grpc v1.77.0
 	google.golang.org/protobuf v1.36.10
@@ -24,7 +25,6 @@ require (
 	github.com/goccy/go-yaml v1.18.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
@@ -34,6 +34,7 @@ require (
 	github.com/quic-go/quic-go v0.57.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.20.0 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"OnnxDetServer/engine"
 	iface "OnnxDetServer/interface"
 	"context"
-	"encoding/base64"
 	"errors"
 	"fmt"
 	"log"
@@ -20,21 +19,15 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"gocv.io/x/gocv"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"gopkg.in/yaml.v3"
 )
 
-type EngineInitRequest struct {
-	ModelPath   string   `json:"modelName" binding:"required"`
-	Names       []string `json:"names" binding:"required"`
-	Threads     int      `json:"threads"`
-	Conf        float32  `json:"conf"`
-	Iou         float32  `json:"iou"`
-	UseGPU      bool     `json:"useGPU"`
-	Description string   `json:"description"`
-}
+var Logger *zap.Logger
 
 type configStruct struct {
 	Port       int `yaml:"port"`
@@ -69,72 +62,30 @@ func (d *WorkerID) add2Seq(detector *engine.Detector, description string, engine
 	d.EngineType = engineType
 	UUID := uuid.New().String()
 	dSequences[UUID] = *d
-	fmt.Printf("Detector %s added with ID %s\n", description, UUID)
+	output := fmt.Sprintf("Detector %s added with ID %s\n", description, UUID)
+	Logger.Info(output)
 	return UUID
 }
 
 // Base64ToMat 将 base64 字符串（可带 data:image/... 前缀）转为 gocv.Mat
-func Base64ToMat(b64 any) (gocv.Mat, error) {
+func Byte64ToMat(b64 []byte) (gocv.Mat, error) {
 	// 去掉可能的 data URL 前缀
-	switch b64.(type) {
-	case string:
-		b64 := b64.(string)
-		if i := strings.Index(b64, ","); i != -1 && strings.HasPrefix(b64, "data:") {
-			b64 = b64[i+1:]
-		}
-
-		data, err := base64.StdEncoding.DecodeString(b64)
+	mat, _ := gocv.IMDecode(b64, gocv.IMReadColor)
+	if mat.Empty() {
+		// IMDecode 返回空 Mat 表示解码失败
+		err := mat.Close()
 		if err != nil {
-			return gocv.NewMat(), err
+			return gocv.Mat{}, err
 		}
-
-		mat, _ := gocv.IMDecode(data, gocv.IMReadColor)
-		if mat.Empty() {
-			// IMDecode 返回空 Mat 表示解码失败
-			err := mat.Close()
-			if err != nil {
-				return gocv.Mat{}, err
-			}
-			return gocv.NewMat(), errors.New("decoded image is empty or unsupported format")
-		}
-		return mat, nil
-	case []byte:
-		mat, _ := gocv.IMDecode(b64.([]byte), gocv.IMReadColor)
-		if mat.Empty() {
-			// IMDecode 返回空 Mat 表示解码失败
-			err := mat.Close()
-			if err != nil {
-				return gocv.Mat{}, err
-			}
-			return gocv.NewMat(), errors.New("decoded image is empty or unsupported format")
-		}
-		return mat, nil
-	default:
-		return gocv.NewMat(), errors.New("input must be a base64 string or byte slice")
+		return gocv.NewMat(), errors.New("decoded image is empty or unsupported format")
 	}
+	return mat, nil
 }
 
 type jobPackage struct {
 	worker backend
-	image  any
+	image  []byte
 	Result chan jobResult
-}
-
-type Position struct {
-	X, Y float32
-}
-
-type Box struct {
-	LT Position
-	RT Position
-	RB Position
-	LB Position
-}
-
-type result struct {
-	Conf   float32
-	Box    Box
-	Center Position
 }
 
 type jobResult struct {
@@ -152,7 +103,8 @@ func startWorker(workerNum int) {
 func runWorker(workerID int) {
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Printf("Worker %d panic: %v. Restarting in 1s...\n", workerID, r)
+			output := fmt.Sprintf("Worker %d panic: %v. Restarting in 1s...\n", workerID, r)
+			Logger.Error(output)
 			//重启这个 Worker
 			time.Sleep(1 * time.Second)
 			go runWorker(workerID)
@@ -160,11 +112,12 @@ func runWorker(workerID int) {
 	}()
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
-	fmt.Printf("---Worker %d created\n", workerID)
+	output := fmt.Sprintf("---Worker %d created\n", workerID)
+	Logger.Info(output)
 	for job := range jobQueue {
 		detector := job.worker
 		image := job.image
-		imgData, err := Base64ToMat(image)
+		imgData, err := Byte64ToMat(image)
 		if err != nil {
 			job.Result <- jobResult{Data: iface.RetData{}}
 		} else {
@@ -173,7 +126,8 @@ func runWorker(workerID int) {
 		}
 		err = imgData.Close()
 		if err != nil {
-			fmt.Printf("⚠️ Worker %d: error closing imgData: %v\n", workerID, err)
+			output = fmt.Sprintf("⚠️ Worker %d: error closing imgData: %v\n", workerID, err)
+			Logger.Error(output)
 		}
 	}
 }
@@ -198,6 +152,7 @@ func (s *server) InitEngine(ctx context.Context, req *pb.InitEngineRequest) (*pb
 	mapMu.Lock()
 	Id := seqdet.add2Seq(&detector, req.Description, int(req.EngineType))
 	mapMu.Unlock()
+	Logger.Info("Initialized new engine", zap.String("ID", Id), zap.String("ModelPath", req.ModelPath), zap.Float32("Confidence", req.Confidence), zap.Float32("IoU", req.Iou), zap.Bool("UseGPU", req.UseGpu))
 	return &pb.InitEngineResponse{
 		Success: true,
 		Id:      Id,
@@ -225,22 +180,24 @@ func (s *server) Inference(ctx context.Context, req *pb.InferenceRequest) (*pb.I
 	results := <-inferResult
 
 	if results.Data.Data == nil {
+		Logger.Error("detector returned nil result")
 		return &pb.InferenceResponse{
 			Success: false,
 			Results: make([]*pb.SingleResult, 0),
 		}, nil
 	}
-	switch results.Data.Data.(type) {
+	switch v := results.Data.Data.(type) {
 	case string:
 		{
+			Logger.Error("detector returned not supported string result:", zap.String("data", v))
 			return &pb.InferenceResponse{
 				Success: false,
 				Results: make([]*pb.SingleResult, 0),
 			}, nil
 		}
-	case map[string][]result:
+	case map[string][]iface.Result:
 		{
-			detResults := results.Data.Data.(map[string][]result)
+			detResults := results.Data.Data.(map[string][]iface.Result)
 			singleResults := make([]*pb.SingleResult, 0, len(detResults))
 			for class, resList := range detResults {
 				for _, res := range resList {
@@ -265,6 +222,8 @@ func (s *server) Inference(ctx context.Context, req *pb.InferenceRequest) (*pb.I
 		}
 	default:
 		{
+			output := fmt.Sprintf("Unknown type: %T", v)
+			Logger.Error(output)
 			return &pb.InferenceResponse{
 				Success: false,
 				Results: make([]*pb.SingleResult, 0),
@@ -280,11 +239,13 @@ func (s *server) DestroyEngine(ctx context.Context, req *pb.DestroyEngineRequest
 	detector, exists := dSequences[UUID]
 	if !exists {
 		mapMu.Unlock()
+		Logger.Error("detector not found with ID", zap.String("ID", UUID))
 		return nil, fmt.Errorf("detector with ID %s not found", UUID)
 	}
 	detector.detector.Destroy()
 	delete(dSequences, UUID)
 	mapMu.Unlock()
+	Logger.Info("Destroyed engine", zap.String("ID", UUID))
 	return &pb.DestroyEngineResponse{
 		Success: true,
 		Message: "Detector destroyed successfully",
@@ -301,13 +262,15 @@ func (s *server) CheckEngine(ctx context.Context, req *pb.CheckEngineRequest) (*
 	}
 	Dconfig := detector.detector.CheckConfig()
 	names := make([]string, 0)
-	switch Dconfig.Names.Data.(type) {
+	switch v := Dconfig.Names.Data.(type) {
 	case []string:
 		names = Dconfig.Names.Data.([]string)
 	case string:
 		names = []string{}
 		names = append(names, "From File")
 	default:
+		output := fmt.Sprintf("Unknown type: %T", v)
+		Logger.Error(output)
 		return nil, fmt.Errorf("unexpected type for names: %T", Dconfig.Names.Data)
 	}
 	ret := &pb.EngineInfo{
@@ -334,7 +297,7 @@ func (s *server) CheckAllEngine(ctx context.Context, req *emptypb.Empty) (*pb.Ch
 	engineInfos := make([]*pb.EngineInfo, 0, len(allSeq))
 	for id, detector := range allSeq {
 		Dconfig := detector.detector.CheckConfig()
-		names := make([]string, 0, len(Dconfig.Names.Data.([]string)))
+		names := make([]string, 0)
 		switch Dconfig.Names.Data.(type) {
 		case []string:
 			names = Dconfig.Names.Data.([]string)
@@ -365,6 +328,7 @@ func (s *server) CheckAllEngine(ctx context.Context, req *emptypb.Empty) (*pb.Ch
 
 func (s *server) Shutdown(ctx context.Context, req *emptypb.Empty) (*emptypb.Empty, error) {
 	go func() {
+		time.Sleep(2 * time.Second)
 		mapMu.Lock()
 		for id, detector := range dSequences {
 			detector.detector.Destroy()
@@ -376,10 +340,34 @@ func (s *server) Shutdown(ctx context.Context, req *emptypb.Empty) (*emptypb.Emp
 		time.Sleep(1 * time.Second)
 		os.Exit(0)
 	}()
+	Logger.Warn("Shutting down in 1 second...")
 	return &emptypb.Empty{}, nil
 }
 
 func main() {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:    "timestamp",
+		LevelKey:   "level",
+		MessageKey: "msg",
+		EncodeLevel: func(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendString("[" + l.String() + "]")
+		},
+		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendString("[" + t.Format("2006-01-02 15:04:05") + "]")
+		},
+		EncodeCaller:   zapcore.FullCallerEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		LineEnding:     zapcore.DefaultLineEnding,
+	}
+
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),
+		zapcore.AddSync(os.Stdout),
+		zapcore.InfoLevel,
+	)
+	Logger = zap.New(core)
+	defer Logger.Sync()
+	// 设置 Gin 为发布模式
 	gin.SetMode(gin.ReleaseMode)
 	fmt.Println(strings.Repeat("#", 64))
 	CPUNum := runtime.NumCPU()
