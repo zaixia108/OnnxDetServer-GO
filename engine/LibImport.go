4a016767ebab54ca5fb6116e1a0c1743e3373c23
zaixia108 <xvbowen2012@gmail.com>
2026-01-12 21:27:09 +0800

    修改至Linux后端

--- engine/LibImport.go
+++ engine/LibImport.go
@@ -1,233 +1,108 @@
 package engine
 
-import (
-	"errors"
-	"fmt"
-	"math"
-	"os"
-	"path/filepath"
-	"runtime"
-	"syscall"
-	"time"
-	"unsafe"
-
-	"gopkg.in/yaml.v3"
-)
-
-type BackendConfig struct {
-	UseBackend     string `yaml:"useBackend"`
-	BackendDir     string `yaml:"backendDir"`
-	BackendLibName string `yaml:"backendLibName"`
-}
-
-var backendCfg BackendConfig
-
-func loadBackendWithDepsWin64(dllDir, dllName string) (*syscall.LazyDLL, error) {
-	k32 := syscall.NewLazyDLL("kernel32.dll")
-	procSetDllDirectoryW := k32.NewProc("SetDllDirectoryW")
-	ptr, err := syscall.UTF16PtrFromString(dllDir)
-	if err != nil {
-		return nil, err
-	}
-	ret, _, callErr := procSetDllDirectoryW.Call(uintptr(unsafe.Pointer(ptr)))
-	if ret == 0 {
-		old := os.Getenv("PATH")
-		_ = os.Setenv("PATH", dllDir+";"+old)
-		if callErr != nil && !errors.Is(callErr, syscall.Errno(0)) {
-			return nil, fmt.Errorf("SetDllDirectoryW failed: %v", callErr)
-		}
-	}
-	dllPath := filepath.Join(dllDir, dllName)
-	mod := syscall.NewLazyDLL(dllPath)
-	if err := mod.Load(); err != nil {
-		return nil, fmt.Errorf("load %s failed: %w", dllPath, err)
-	}
-	return mod, nil
-}
-
-var (
-	mod                *syscall.LazyDLL
-	procCreate         *syscall.LazyProc
-	procDestroy        *syscall.LazyProc
-	procInit           *syscall.LazyProc
-	procDetect         *syscall.LazyProc
-	procReleaseResults *syscall.LazyProc
-	procSetInputSize   *syscall.LazyProc
-	procSetBlobName    *syscall.LazyProc
-)
-
-func detArch(system, arch string) string {
-	switch arch {
-	case "amd64":
-		{
-			return fmt.Sprintf("%s-%s", system, "x64")
-		}
-	case "386":
-		{
-			return fmt.Sprintf("%s-%s", system, "x86")
-		}
-	case "arm64":
-		{
-			return fmt.Sprintf("%s-%s", system, "arm64")
-		}
-	default:
-		panic(fmt.Sprintf("Architecture %s not supported", arch))
-	}
-}
-
-func getPlatform() string {
-	system := runtime.GOOS
-	arch := runtime.GOARCH
-	switch system {
-	case "windows":
-		return detArch(system, arch)
-	case "darwin":
-		panic("MacOS is not supported")
-	case "linux":
-		return detArch(system, arch)
-	default:
-		panic(fmt.Sprintf("Operating system %s not supported", system))
-	}
-}
-
-func init() {
-	var err error
-	Platform := getPlatform()
-	// 获取可执行文件的路径
-	configData, err := os.ReadFile("src/backend.yaml")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to read backend.yaml: %v\n", err)
-		time.Sleep(5 * time.Second)
-		os.Exit(1)
-	}
-	err = yaml.Unmarshal(configData, &backendCfg)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to parse backend.yaml: %v\n", err)
-		time.Sleep(5 * time.Second)
-		os.Exit(1)
-	}
-	exePath, err := os.Executable()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to get executable path: %v\n", err)
-		os.Exit(1)
-	}
-	// 基于可执行文件路径构建 'src' 目录的绝对路径
-	exeDir := filepath.Dir(exePath)
-	dllDir := filepath.Join(exeDir, backendCfg.BackendDir)
-	if Platform == "linux-x64" {
-		panic("Linux is not supported yet, developing...")
-	} else {
-		mod, err = loadBackendWithDepsWin64(dllDir, backendCfg.BackendLibName)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to load DLLs from '%s': %v\nEnsure `src` directory with DLLs exists next to the executable, and install Visual C++ Redistributable.\n", dllDir, err)
-			os.Exit(1)
-		}
-	}
-	procCreate = mod.NewProc("CreateDetector")
-	procDestroy = mod.NewProc("DestroyDetector")
-	procInit = mod.NewProc("InitDetector")
-	procDetect = mod.NewProc("Detect")
-	procReleaseResults = mod.NewProc("ReleaseResults")
-	if backendCfg.UseBackend == "ncnn" {
-		procSetInputSize = mod.NewProc("SetInputSize")
-		procSetBlobName = mod.NewProc("SetBlobName")
-	}
-}
+/*
+#CGO_ENABLED=1
+#cgo CFLAGS: -Isrc
+#cgo LDFLAGS: -Lsrc -lNcnnDet
+#include "src/NcnnDet.h"
+*/
+import "C"
+import "unsafe"
 
 func CreateDetector() unsafe.Pointer {
-	if procCreate == nil {
-		return nil
-	}
-	r, _, _ := procCreate.Call()
-	return unsafe.Pointer(r)
+	detector := C.CreateDetector()
+	return unsafe.Pointer(detector)
 }
 
 func DestroyDetector(p unsafe.Pointer) {
-	if p == nil || procDestroy == nil {
+	if p == nil {
 		return
 	}
-	procDestroy.Call(uintptr(p))
+	C.DestroyDetector((*C.Detector)(p))
 }
 
 func InitDetector(p unsafe.Pointer, modelPath string, conf, iou float32, useGPU bool) bool {
-	if p == nil || procInit == nil {
+	if p == nil {
 		return false
 	}
-	mp, _ := syscall.BytePtrFromString(modelPath)
-	var ug uintptr
+	cModelPath := C.CString(modelPath)
+	defer C.free(unsafe.Pointer(cModelPath))
+	var ug C.bool
 	if useGPU {
-		ug = 1
+		ug = C.bool(true)
+	} else {
+		ug = C.bool(false)
 	}
-	r, _, _ := procInit.Call(
-		uintptr(p),
-		uintptr(unsafe.Pointer(mp)),
-		uintptr(math.Float32bits(conf)),
-		uintptr(math.Float32bits(iou)),
-		ug,
-	)
-	return r != 0
+	ret := C.InitDetector((*C.Detector)(p), cModelPath, C.float(conf), C.float(iou), ug)
+	return bool(ret)
 }
 
 func Detect(detector unsafe.Pointer, imageData []byte, width, height, channels int) (boxes []float32, scores []float32, classes []int32, count int32, ok bool) {
-	if detector == nil || len(imageData) == 0 || procDetect == nil {
+	if detector == nil || len(imageData) == 0 {
 		return
 	}
+	var outBoxesPtr *C.float
+	var outScoresPtr *C.float
+	var outClassesPtr *C.int
+	var outCount C.int
 
-	var outBoxesPtr, outScoresPtr, outClassesPtr uintptr
-	var outCount int32
-
-	r, _, _ := procDetect.Call(
-		uintptr(detector),
-		uintptr(unsafe.Pointer(&imageData[0])),
-		uintptr(width),
-		uintptr(height),
-		uintptr(channels),
-		uintptr(unsafe.Pointer(&outBoxesPtr)),
-		uintptr(unsafe.Pointer(&outScoresPtr)),
-		uintptr(unsafe.Pointer(&outClassesPtr)),
-		uintptr(unsafe.Pointer(&outCount)),
+	ret := C.Detect(
+		(*C.Detector)(detector),
+		(*C.uchar)(unsafe.Pointer(&imageData[0])),
+		C.int(width),
+		C.int(height),
+		C.int(channels),
+		&outBoxesPtr,
+		&outScoresPtr,
+		&outClassesPtr,
+		&outCount,
 	)
-	ok = r != 0
-	count = outCount
+	ok = bool(ret)
+	count = int32(outCount)
 	if !ok || count == 0 {
 		return
 	}
 
-	tmpBoxes := unsafe.Slice((*float32)(unsafe.Pointer(outBoxesPtr)), int(count*4))
-	tmpScores := unsafe.Slice((*float32)(unsafe.Pointer(outScoresPtr)), int(count))
-	tmpClasses := unsafe.Slice((*int32)(unsafe.Pointer(outClassesPtr)), int(count))
+	boxes = make([]float32, count*4)
+	scores = make([]float32, count)
+	classes = make([]int32, count)
 
-	boxes = append([]float32(nil), tmpBoxes...)
-	scores = append([]float32(nil), tmpScores...)
-	classes = append([]int32(nil), tmpClasses...)
+	boxesSlice := unsafe.Slice((*C.float)(unsafe.Pointer(outBoxesPtr)), int(count*4))
+	scoresSlice := unsafe.Slice((*C.float)(unsafe.Pointer(outScoresPtr)), int(count))
+	classesSlice := unsafe.Slice((*C.int)(unsafe.Pointer(outClassesPtr)), int(count))
 
-	if procReleaseResults != nil {
-		procReleaseResults.Call(outBoxesPtr, outScoresPtr, outClassesPtr)
+	for i := 0; i < int(count); i++ {
+		scores[i] = float32(scoresSlice[i])
+		classes[i] = int32(classesSlice[i])
+		for j := 0; j < 4; j++ {
+			boxes[i*4+j] = float32(boxesSlice[i*4+j])
+		}
 	}
+
+	// 释放 C 语言分配的内存
+	C.FreeMemory(unsafe.Pointer(outBoxesPtr))
+	C.FreeMemory(unsafe.Pointer(outScoresPtr))
+	C.FreeMemory(unsafe.Pointer(outClassesPtr))
+
 	return
 }
 
 func SetInputSize(detector unsafe.Pointer, size int) {
-	if detector == nil || procSetInputSize == nil {
+	if detector == nil {
 		return
 	}
-	_, _, _ = procSetInputSize.Call(
-		uintptr(detector),
-		uintptr(size),
-	)
+	C.SetInputSize((*C.Detector)(detector), C.int(size))
 	return
 }
 
-func SetBlobName(detector unsafe.Pointer, inputBlobName, outputBlobName string) {
-	if detector == nil || procSetBlobName == nil {
+func SetBlobName(detector unsafe.Pointer, inputName, outputName string) {
+	if detector == nil {
 		return
 	}
-	inBlobPtr, _ := syscall.BytePtrFromString(inputBlobName)
-	outBlobPtr, _ := syscall.BytePtrFromString(outputBlobName)
-	_, _, _ = procSetBlobName.Call(
-		uintptr(detector),
-		uintptr(unsafe.Pointer(inBlobPtr)),
-		uintptr(unsafe.Pointer(outBlobPtr)),
-	)
+	cInputName := C.CString(inputName)
+	defer C.free(unsafe.Pointer(cInputName))
+	cOutputName := C.CString(outputName)
+	defer C.free(unsafe.Pointer(cOutputName))
+	C.SetBlobName((*C.Detector)(detector), cInputName, cOutputName)
 	return
 }
